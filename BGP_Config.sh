cbe98291450e66374ce14355d32cb42d5fcfab2b
Bretonator <bretlinne@gmail.com>
2017-11-10 22:07:36 -0800

    Added the BGP_Config.sh
    This is a BASH shell script that just invokes all the playbooks in a row.  The user will be prompted for the vault-password.

---
+++ BGP_Config.sh
@@ -0,0 +1,6 @@
+ansible-playbook vlan_playbook.yml -i inventory --ask-vault-pass
+ansible-playbook selfip_playbook.yml -i inventory --ask-vault-pass
+ansible-playbook routedomain_playbook.yml -i inventory --ask-vault-pass
+ansible-playbook vServer_playbook.yml -i inventory --ask-vault-pass
+ansible-playbook vAddr_playbook.yml -i inventory --ask-vault-pass
+ansible-playbook command_playbook.yml -i inventory --ask-vault-pass
